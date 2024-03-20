13c2c87b98936760176323a24842b6b37f603047
Brogan Pinheiro <142768386+broganpinheiro@users.noreply.github.com>
2024-03-20 16:47:53 +0000

    Wed, Mar 20, 2024, 9:47 AM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -13,4 +13,39 @@ hiderules.addEventListener('click', () => {
 })
 
 
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
 
+
+// Create ball properties
+ball = {
+    x: canvas.width / 2,
+    y: canvas.height / 2,
+    size: 10,
+    speed: 4,
+    dx: 4,
+    dy: -4,
+}
+
+
+// Draw ball on canvas
+function drawBall() {
+    ctx.beginPath()
+    ctx.arc(ball.x,ball.y,ball.size,0,Math.PI * 2)
+    ctx.fillStyle = '#0095dd'
+    ctx.fill()
+    ctx.closePath()
+}
+
+drawBall()
+
+
+// Create paddle properties
+paddle = {
+    x: canvas.width / 2 - 40,
+    y: canvas.height - 20,
+    w: 80,
+    h: 10,
+    speed: 8,
+    dx: 0,
+}
