1ea70a8f69b70378cc31f63b6feece7b092ee80a
Brogan Pinheiro <142768386+broganpinheiro@users.noreply.github.com>
2024-04-02 16:43:54 +0000

    Tue, Apr 2, 2024, 9:43 AM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -181,7 +181,7 @@ function moveBall() {
     // paddle collision
     if (ball.x - ball.size > paddle.x &&
         ball.x + ball.size < paddle.x + paddle.w &&
-        ball.y + ball.size < paddle.h) {
+        ball.y + ball.size > paddle.y) {
             ball.dy = -1 * ball.dy
     }
 }
