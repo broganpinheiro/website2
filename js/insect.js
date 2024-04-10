c798d7e638cc6ef40d9f15fdc15fc1c5dd96ba85
Brogan Pinheiro <142768386+broganpinheiro@users.noreply.github.com>
2024-04-10 09:58:06 -0700

    test

--- js/insect.js
+++ js/insect.js
@@ -5,6 +5,7 @@ const start_btn = document.getElementById('start-btn')
 const timeEl = document.getElementById('time')
 const scoreEl = document.getElementById('score')
 const message = document.getElementById('message')
+const won = document.getElementById('winning')
 let seconds = 0
 let score = 0
 let selected_insect = {}
@@ -13,14 +14,15 @@ start_btn.addEventListener('click',() => {
     screens[0].classList.add('up')
 })
 
-choose_insectPbtns.forEach(btn => {
+choose_insect_btns.forEach(btn => {
     btn.addEventListener('click', () => {
         const img = btn.querySelector('img')
         const alt = img.getAttribute('alt')
         const src = img.getAttribute('src')
         screens[1].classList.add('up')
         selected_insect = {src,alt}
-        setTimeout(createInsect, 100)
+        setTimeout(createInsect, 1000)
+        startGame()
     })
 })
 
@@ -28,6 +30,51 @@ function createInsect() {
     const insect = document.createElement('div')
     insect.classList.add('insect')
     const {x,y} = getRandomLocation()
+    insect.style.top = `${y}px`
+    insect.style.left = `${x}px`
+    insect.innerHTML = `<img src="${selected_insect.src}" alt=${selected_insect.alt} style = "transform: rotate(${Math.random() * 360}deg)" />`
+
+    insect.addEventListener('click', catchInsect)
+
+    game_container.appendChild(insect)
+}
+
+function catchInsect() {
+    increaseScore()
+    this.classList.add('caught')
+    setTimeout(() => this.remove(), 1000)
+    addInsects()
+}
+
+function addInsects() {
+    setTimeout(createInsect, 1000)
+    setTimeout(createInsect, 1500)
+}
+
+function startGame() {
+    setInterval(increaseTime, 1000)
+}
+
+function increaseTime() {
+    let m = Math.floor(seconds / 60)
+    let s = seconds % 60
+    if (m < 10) {
+        m = `0${m}`
+    }
+    if (s < 10) {
+        s = `0${s}`
+    }
+    timeEl.innerHTML = `Time: ${m}:${s}`
+    seconds++
+    
+}
+
+function increaseScore() {
+    score++
+    if (score > 19) {
+        message.classList.add('visible')
+    }
+    scoreEl.innerHTML = `Score: ${score}`
 }
 
 function getRandomLocation() {
@@ -37,3 +84,6 @@ function getRandomLocation() {
     const y = Math.random() * (height - 200) + 100
     return {x,y}
 }
+
+
+
