3bef92c2d2759d75fc59d6cd163cab9575a8673b
Brogan Pinheiro <142768386+broganpinheiro@users.noreply.github.com>
2024-05-21 18:11:52 +0000

    Tue, May 21, 2024, 11:11 AM -07:00

--- js/insect.js
+++ js/insect.js
@@ -107,10 +107,8 @@ code = document.getElementById('toggle')
 container = document.querySelector('.ccontainer')
 button = document.querySelector('.fa-solid')
 
-code.addEventListener('click', () => {
+start_btn.addEventListener('click', () => {
     container.classList.toggle('inactive')
-    button.classList.toggle('fa-angle-down')
-    button.classList.toggle('fa-angle-up')
 })
 
 
