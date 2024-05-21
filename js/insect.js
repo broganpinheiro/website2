efff84f1d27fbc038a14199e8f3a95367d8a8651
Brogan Pinheiro <142768386+broganpinheiro@users.noreply.github.com>
2024-05-21 09:52:29 -0700

    hope

--- js/insect.js
+++ js/insect.js
@@ -103,11 +103,11 @@ function getRandomLocation() {
 
 
 
-code = document.getElementById('toggle')
+
 container = document.querySelector('.ccontainer')
 button = document.querySelector('.fa-solid')
 
-code.addEventListener('click', () => {
+start_btn.addEventListener('click', () => {
     container.classList.toggle('inactive')
     button.classList.toggle('fa-angle-down')
     button.classList.toggle('fa-angle-up')
